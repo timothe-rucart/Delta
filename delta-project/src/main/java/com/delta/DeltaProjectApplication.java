14c89fc864b608e97d56270f0fedff04849525ca
t.rucart <t.rucart@PC193.domaine.local>
2020-06-14 15:17:45 +0200

    first commit

---
+++ delta-project/src/main/java/com/delta/DeltaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.delta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaProjectApplication.class, args);
+	}
+
+}
---
+++ delta-project/src/test/java/com/delta/DeltaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
