3f2a6b7a1204ba41b3a8afc8b728d60577dc5fb5
t.rucart <t.rucart@PC193.domaine.local>
2020-06-14 15:41:06 +0200

    parametrage de la BDD

--- delta-project/src/main/java/com/delta/DeltaProjectApplication.java
+++ delta-project/src/main/java/com/delta/DeltaProjectApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeltaProjectApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(DeltaProjectApplication.class, args);
 	}
