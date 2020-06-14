8aec5b8dade249fe1353c282f732945efb7262ad
t.rucart <t.rucart@PC193.domaine.local>
2020-06-14 15:23:10 +0200

    commit test

--- delta-project/src/main/java/com/delta/DeltaProjectApplication.java
+++ delta-project/src/main/java/com/delta/DeltaProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeltaProjectApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DeltaProjectApplication.class, args);
 	}
